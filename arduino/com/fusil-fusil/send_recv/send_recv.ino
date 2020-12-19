4ea6d0f247166d4390a0bef46b20fa59999be118
arthurliu2022 <arthur.liu.2022@ensta-paris.fr>
2020-12-19 21:16:34 +0100

    La fonction d'envoi de tir est maintenant asynchrone

--- arduino/com/fusil-fusil/send_recv/send_recv.ino
+++ arduino/com/fusil-fusil/send_recv/send_recv.ino
@@ -8,15 +8,22 @@ int lastReceiverState = 0;
 int triggerState = 0;
 int lastTriggerState = 0;
 
-String id = "010100101";
+bool isSendingPacket = false; //Si l'émetteur est en train d'envoyer un message
+bool isSendingBit = false;
+int currentSendStep = 0; //Indice du bit en train d'être émis dans le message
+bool currentSendState = 0; //Si l'émetteur est actuellement allumé
 
-unsigned long chrono;
+//Format standard message : "s******e", * = 0 ou 1
+String id = "s1010e";
+
+unsigned long recvChrono;
+unsigned long sendChrono;
 unsigned long highTime; //durée du signal en position haute
 
 //Durée des signaux (µs)
 unsigned int zeroDuration = 600;
 unsigned int oneDuration = 1200;
-unsigned int headerDuration = 2400;
+unsigned int startDuration = 2400;
 unsigned int endDuration = 1800;
 
 //Tolérance dans la durée des impulsions reçues (µs)
@@ -31,7 +38,7 @@ void readBit(unsigned long highTime) {
   else if (oneDuration - e < highTime && highTime < oneDuration + e) {
     Serial.print("1");
   }
-  else if (headerDuration - e < highTime && highTime < headerDuration + e) {
+  else if (startDuration - e < highTime && highTime < startDuration + e) {
     Serial.print("Transmission start\n");
   }
   else if (endDuration - e < highTime && highTime < endDuration + e) {
@@ -41,39 +48,46 @@ void readBit(unsigned long highTime) {
 
 
 
-//Envoi du signal de début de message
-void sendHeader(int pin) { 
-  digitalWrite(pin, HIGH);
-  delayMicroseconds(headerDuration);
-  digitalWrite(pin, LOW);
-  delayMicroseconds(zeroDuration);
-}
-
-
-
-//Envoi du signal de fin de message
-void sendEnd(int pin) { 
-  digitalWrite(pin, HIGH);
-  delayMicroseconds(endDuration);
-  digitalWrite(pin, LOW);
-  delayMicroseconds(zeroDuration);
-}
-
-
-
-//Envoi de 1 ou 0
-void sendBit(int pin, char bit) {
-  if (bit == '0') { //Envoi du 0
-    digitalWrite(pin, HIGH);
-    delayMicroseconds(zeroDuration);
-    digitalWrite(pin, LOW);
-    delayMicroseconds(zeroDuration);
+//Envoi de 0, 1, début de message, fin de message
+void sendBit(int pin, char bit, int len) {
+  unsigned int highDuration = 0;
+  switch (bit) {
+    case '0':
+      highDuration = zeroDuration;
+      break;
+    case '1':
+      highDuration = oneDuration;
+      break;
+    case 's': //Début de message
+      highDuration = startDuration;
+      break;
+    case 'e': //Fin de message
+      highDuration = endDuration;
+      break;
   }
-  else if (bit == '1') { //Envoi du 1
-    digitalWrite(pin, HIGH);
-    delayMicroseconds(oneDuration);
-    digitalWrite(pin, LOW);
-    delayMicroseconds(zeroDuration);
+
+  if (!isSendingBit && !currentSendState) {
+    isSendingBit = true;
+    currentSendState = true;
+    digitalWrite(emitter, HIGH);
+    sendChrono = micros();
+  }
+  else if (currentSendState) {
+    if (micros() - sendChrono >= highDuration) { //Fin du signal haut
+      currentSendState = false;
+      digitalWrite(emitter, LOW);
+      sendChrono = micros();
+    }
+  }
+  else {
+    if (micros() - sendChrono >= zeroDuration) { //Fin du signal bas et fin d'envoi du bit
+      isSendingBit = false;
+      currentSendStep++;
+      if(currentSendStep == len) {
+        isSendingPacket = false;
+        currentSendStep = 0;
+      }
+    }
   }
 }
 
@@ -81,26 +95,22 @@ void sendBit(int pin, char bit) {
 
 void sendPacket(int pin, String packet) {
   int len = packet.length();
-  sendHeader(pin);
-  for(int i = 0; i<len; i++) {
-    sendBit(pin, packet[i]);
-  }
-  sendEnd(pin);
+    sendBit(pin, packet[currentSendStep], len);
 }
 
 
 
 //Détecter l'appui sur la gâchette
-void checkTrigger(int trigger) {
+void checkTrigger() {
   triggerState = digitalRead(trigger);
   if (triggerState == 0) { //On n'appuie pas sur la gâchette
     if (lastTriggerState == 1) { //Front descendant (on a lâché la gâchette)
       lastTriggerState = 0;
     }
   }
-  else if (triggerState == 1) { //On appuie sur la gâchette
+  else if (triggerState == 1 && !isSendingPacket) { //On appuie sur la gâchette
     if (lastTriggerState == 0) { //Front montant (on vient d'appuyer sur la gâchette)
-      sendPacket(emitter, id);
+      isSendingPacket = true;
       lastTriggerState = 1;
     }
   }
@@ -112,13 +122,13 @@ void checkReceiver() {
   receiverState = digitalRead(receiver);
   if (receiverState == 1) { //On capte un signal
     if (lastReceiverState == 0) { //Front montant
-      chrono = micros();
+      recvChrono = micros();
       lastReceiverState = 1;
     }
   }
   else if (receiverState == 0) { //On ne capte pas de signal
     if (lastReceiverState == 1) { //Front descendant
-      highTime = micros() - chrono;
+      highTime = micros() - recvChrono;
       readBit(highTime);
       lastReceiverState = 0;
     }
@@ -136,5 +146,8 @@ void setup() {
 
 void loop() {
   checkReceiver();
-  checkTrigger(trigger);
+  checkTrigger();
+  if (isSendingPacket) {
+    sendPacket(emitter, id);
+  }
 }
