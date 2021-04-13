12ddfac2a00a15887ed27a4e37751dcd56e9197d
aulaforge <62746663+aulaforge@users.noreply.github.com>
2021-04-13 15:29:23 +0200

    Add files via upload

--- Statistiques/interface.cpp
+++ Statistiques/interface.cpp
@@ -6,10 +6,20 @@
 #include "statsEquipe.h"
 #include "interface.h"
 
-void writeStats(ofstream file, Player player){
+void writeStatsPerso(std::ofstream file, Player player){
   file << "Personal statistics : \n";
   file << "Player name : " << player.getName() << "\n";
   file << "Score : " << player.getScore() << "\n";
   file << "Member of the team n° : " << player.getTeam() << "\n";
 
+}
+
+void writeStatsTeam(std::ofstream file, Team team){
+  int size = team.getSize();
+  std::vector<Player*> members = team.getMembers();
+  file << "Statistics of the team number "<< team.getId() <<" :\n";
+  file << "Score : " << team.getScore() << "\n";
+  for (int i=0; i<size; i++){
+    file << "\t" << "Player name : " << (*(members[i])).getName() <<" / "<< "Score : " << (*(members[i])).getScore() << "\n";
+  }
 }
