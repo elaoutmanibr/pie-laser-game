9dfa1c9775c3e4156a68c39ceab95ed3d74da42d
liam-kelley <liam.kelley.144@gmail.com>
2022-03-11 15:15:05 +0100

    base serveur.ino

--- 2021/Serveur/Serveur.ino
+++ 2021/Serveur/Serveur.ino
@@ -1,9 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
 
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,16,2); // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+
+void setup(){
+  lcd.init();
+  lcd.backlight();
+  lcd.print("Bienvenue au");
+  lcd.setCursor(0,1);         //Display position
+  lcd.print(" PIE  LAZERGAME ");
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop(){
+  // Turn off the display:
+  lcd.noDisplay();
+  delay(500);
+  // Turn on the display:
+  lcd.display();
+  delay(500);
 }
