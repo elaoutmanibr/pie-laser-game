a44546a4207a26a37d08adfce8e0ddac39e5c71c
liam-kelley <liam.kelley.144@gmail.com>
2022-04-08 15:20:34 +0200

    formatting test radio v2

--- 2021/Serveur/Serveur.ino
+++ 2021/Serveur/Serveur.ino
@@ -38,15 +38,13 @@ Player players[2] = {p1,p2};
 void setup(){
   lcd.init();
   lcd.backlight();
+  lcd.print("Bienvenue au");
+  lcd.setCursor(0,1);         //Display position
+  lcd.print(" TEST RADIO V2 ");
   vw_set_rx_pin(RF_RX_PIN);
   vw_set_tx_pin(RF_TX_PIN);
   vw_setup(2000);
   vw_rx_start();
-
-
-  lcd.print("Bienvenue au");
-  lcd.setCursor(0,1);         //Display position
-  lcd.print(" TEST RADIO V2 ");
 }
 void loop(){
   // Turn off the display:
@@ -55,20 +53,25 @@ void loop(){
   // Turn on the display:
   //lcd.display();
   //delay(500);
-  lcd.setCursor(0,0);
-  lcd.print("AWAITING MSSG   ");
+  //lcd.setCursor(0,0);
+  //lcd.print("AWAITING MSSG   ");
 
   //vw_wait_rx_max(500);
   if (vw_get_message(message, &size_msg)) {
     Shot shot;
-    lcd.print("MSSG RECEIVED   ");
+    //lcd.print("MSSG RECEIVED   ");
     if(message[0]==16){    // 16 => msg hit
+      
       //Read what shot it is
       shot.shooter = message[1];
       shot.victim = message[2];
+      
       //BDD update
-      players[shot.shooter].hits_emitted++;
-      players[shot.victim].hits_received++;
+      if(players[shot.shooter].hits_emitted<254){players[shot.shooter].hits_emitted++;}
+      else{players[shot.shooter].hits_emitted=0;}
+      if(players[shot.victim].hits_received<254){players[shot.victim].hits_received++;}
+      else{players[shot.victim].hits_received=0;}
+      
       //lcd quick update (debug)
       lcd.setCursor(0,0);
       lcd.print("SHOT            ");
@@ -78,11 +81,13 @@ void loop(){
       lcd.print(players[shot.victim].hits_received);
       lcd.setCursor(0,1);
       lcd.print(" TEST RADIO V2 ");
-      delay(500);
+      delay(200);
+      
       //Set outward message to victim has been shot
       response[0]=  shot.victim; // id du victim
       response[1]=20 ;// 20 => msg tir reçu/début paralysie
       response[2]=players[shot.victim].hits_received; //How many hits that makes, total
+      
       //Attente de confirmation
       vw_send(response, 3);
       vw_wait_tx(); // attendre la fin de l'envoi
