2d870131d291d3d2cf45e21ea33ec30f36095075
liam-kelley <liam.kelley.144@gmail.com>
2022-04-08 14:23:42 +0200

    help

--- 2021/Pistolet/Pistolet.ino
+++ 2021/Pistolet/Pistolet.ino
@@ -1,6 +1,6 @@
 #include <LiquidCrystal_I2C.h>
 #include <VirtualWire.h>
-#include <IRremote.h> // Referring to the IRRemote function library, the header file 
+//#include <IRremote.h> // Referring to the IRRemote function library, the header file 
                         //has defined PIN 3 as the signal output, so it can only connect 
                         //to PIN 3. If change, please change it in the header file
 
@@ -8,13 +8,12 @@ byte ID = 0; // identifiant du pistolet
 byte hits_recv =0 ;
 
 LiquidCrystal_I2C lcd(0x27,16,2); // set the LCD address to 0x27 for a 16 chars and 2 line display
-int RF_RX_PIN = 9;
-int RF_TX_PIN = 11;
-const int buttonPin = 2;   
+int RF_RX_PIN = 10;
+int RF_TX_PIN = 12;
+const int buttonPin = 4;   
 byte message[3];
 byte size_msg =3;
 byte response[3];
-
 
 void setup(){
   lcd.init();
@@ -30,7 +29,6 @@ void setup(){
 }
 
 void loop() {
-  buttonState = digitalRead(buttonPin);
   if (vw_get_message(message, &size_msg) && message[0]==ID && message[1]==20) { // 20 : code paralysie
     // Affichage paralysie
     delay(15*1000);
@@ -44,7 +42,7 @@ void loop() {
 
   }
 
-  if (buttonState == HIGH) { // si le bouton hit recieved est appuyé dans la simulation
+  if (digitalRead(buttonPin) == HIGH) { // si le bouton hit recieved est appuyé dans la simulation
       response[0]=16; // code hit recieved
       response[1]=ID;
       response[2]=1; // id attaquant
